ca923d345a4e81b9a01e0d6f0475b5185ef23171
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2023-10-25 23:00:45 +0000

    completed lab

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,20 @@
 import { useState } from "react";
-
+ 
 function UpdatedForm() {
   const [formData, setFormData] = useState({
     firstName: "joyce",
     lastName: "mwangi",
+    admin: false,
   });
 
   function handleChange(e) {
     const name = e.target.name;
     let value = e.target.value;
 
-   
+    
+    if (e.target.type === "checkbox") {
+      value = e.target.checked;
+    }
 
     setFormData({
       ...formData,
@@ -25,21 +29,24 @@ function UpdatedForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        id="firstName" 
-        name="firstName" 
-        value={formData.firstName}
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        id="lastName" 
-        name="lastName" 
-        value={formData.lastName}
-        onChange={handleChange}
-      />
-   
+    <input
+      type="text"
+      name="firstName"
+      onChange={handleChange}
+      value={formData.firstName}
+    />
+    <input
+      type="text"
+      name="lastName"
+      onChange={handleChange}
+      value={formData.lastName}
+    />
+    <input
+      type="checkbox"
+      name="admin"
+      onChange={handleChange}
+      checked={formData.admin}
+    />
       <button type="submit">Submit</button>
     </form>
   );
